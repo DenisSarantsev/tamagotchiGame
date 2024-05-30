b9e9a8a51de5a2fbc5ee762d49d8b37de55cfe03
Denis <sarantsev.denis.94@gmail.com>
2024-05-31 01:13:37 +0300

    Завершен блок с колесом

--- dist/js/app.js
+++ dist/js/app.js
@@ -951,8 +951,78 @@
             function onVisibilityChange(entries, observer) {
                 entries.forEach((entry => {
                     if (entry.isIntersecting) {
-                        rotateWheel.style.animation = "wheelRotateAnimation 10s ease-in-out 2s";
-                        rotateEggs.style.animation = "eggsRotateAnimation 10s ease-in-out 2s";
+                        const backgroundLights = document.querySelector(".jackpot-block__lights");
+                        backgroundLights.style.animation = "showObjectAfterLazyLoading 3s linear";
+                        setTimeout((() => {
+                            backgroundLights.style.opacity = "1";
+                        }));
+                        const rotateObject = object => {
+                            object.style.animation = "wheelRotateAnimation 10s cubic-bezier(.39,.01,.49,.99)";
+                        };
+                        setTimeout((() => {
+                            rotateObject(rotateWheel);
+                        }), 2e3);
+                        const allEggsShadows = document.querySelectorAll(".eggs-rotate__egg-shadow");
+                        setTimeout((() => {
+                            rotateEggs.style.animation = "wheelRotateAnimation 10s cubic-bezier(.39,.01,.49,.99)";
+                            setTimeout((() => {
+                                rotateEggs.style.animation = "eggsFlyAnimation 1s cubic-bezier(0,1.03,.17,1)";
+                                flyEggs();
+                            }), 5100);
+                        }), 2e3);
+                        const egg1 = document.querySelector(".egg1");
+                        const egg2 = document.querySelector(".egg2");
+                        const egg3 = document.querySelector(".egg3");
+                        const egg4 = document.querySelector(".egg4");
+                        const egg5 = document.querySelector(".egg5");
+                        const egg6 = document.querySelector(".egg6");
+                        const egg7 = document.querySelector(".egg7");
+                        const egg8 = document.querySelector(".egg8");
+                        const egg9 = document.querySelector(".egg9");
+                        const egg10 = document.querySelector(".egg10");
+                        const egg11 = document.querySelector(".egg11");
+                        const egg12 = document.querySelector(".egg12");
+                        let indexFly = 1;
+                        const flyEggs = () => {
+                            egg1.style.animation = "flyEgg1 1s cubic-bezier(0,1.03,.17,1)";
+                            egg2.style.animation = `flyEgg2 1s cubic-bezier(0,1.03,.17,1)`;
+                            egg3.style.animation = "flyEgg3 1s cubic-bezier(0,1.03,.17,1)";
+                            egg4.style.animation = `flyEgg4 1s cubic-bezier(0,1.03,.17,1)`;
+                            egg5.style.animation = "flyEgg5 1s cubic-bezier(0,1.03,.17,1)";
+                            egg6.style.animation = `flyEgg6 1s cubic-bezier(0,1.03,.17,1)`;
+                            egg7.style.animation = "flyEgg7 1s cubic-bezier(0,1.03,.17,1)";
+                            egg8.style.animation = `flyEgg8 1s cubic-bezier(0,1.03,.17,1)`;
+                            egg9.style.animation = "flyEgg9 1s cubic-bezier(0,1.03,.17,1)";
+                            egg10.style.animation = "flyEgg10 1s cubic-bezier(0,1.03,.17,1)";
+                            egg11.style.animation = `flyEgg11 1s cubic-bezier(0,1.03,.17,1)`;
+                            egg12.style.animation = "flyEgg12 1s cubic-bezier(0,1.03,.17,1)";
+                            showJackpotText();
+                            setTimeout((() => {
+                                egg1.style.transform = `translateX(${60 * indexFly}px) translateY(-${200 * indexFly}px)`;
+                                egg2.style.transform = `translateX(${130 * indexFly}px) translateY(-${80 * indexFly}px) translateZ(300px)`;
+                                egg3.style.transform = `translateX(${350 * indexFly}px) translateY(-${60 * indexFly}px)`;
+                                egg4.style.transform = `translateX(${160 * indexFly}px) translateY(${25 * indexFly}px) translateZ(300px)`;
+                                egg5.style.transform = `translateX(${190 * indexFly}px) translateY(${160 * indexFly}px)`;
+                                egg6.style.transform = `translateX(${25 * indexFly}px) translateY(${95 * indexFly}px) translateZ(300px)`;
+                                egg7.style.transform = `translateX(${-40 * indexFly}px) translateY(${170 * indexFly}px)`;
+                                egg8.style.transform = `translateX(${-120 * indexFly}px) translateY(${110 * indexFly}px) translateZ(300px)`;
+                                egg9.style.transform = `translateX(${-280 * indexFly}px) translateY(${80 * indexFly}px)`;
+                                egg10.style.transform = `translateX(${-320 * indexFly}px) translateY(${-70 * indexFly}px)`;
+                                egg11.style.transform = `translateX(${-130 * indexFly}px) translateY(${-100 * indexFly}px) translateZ(300px)`;
+                                egg12.style.transform = `translateX(${-40 * indexFly}px) translateY(${-160 * indexFly}px)`;
+                                for (let item of allEggsShadows) item.style.animation = "allEggsShadows 2s linear";
+                                setTimeout((() => {
+                                    for (let item of allEggsShadows) item.classList.add("egg-shadow");
+                                }), 2e3);
+                            }), 1e3);
+                        };
+                        const jackpotTextBlock = document.querySelector(".jackpot-text");
+                        const showJackpotText = () => {
+                            jackpotTextBlock.style.animation = "showJackpotText 2s linear";
+                            setTimeout((() => {
+                                jackpotTextBlock.style.opacity = "1";
+                            }), 2e3);
+                        };
                         addAnimationToBigSparks();
                         addAnimationToSmallSparks();
                         showObjectAfterLazyLoading(wheelAndEggsContainer);
